61390f10f644d46fe7e4a199fc25fd0491a71286
Max Kagamine <max@kgmn.jp>
2020-05-30 11:53:44 -0700

    Polyfilled Intl.PluralRules & Intl.RelativeTimeFormat

--- plugins/localized-pages/gatsby-node.js
+++ plugins/localized-pages/gatsby-node.js
@@ -1,5 +1,6 @@
 const parser = require('intl-messageformat-parser');
 const mapValues = require('lodash.mapvalues');
+const webpack = require('webpack');
 
 let parsedMessages = new Map();
 
@@ -33,4 +34,25 @@ exports.onCreatePage = async ({ page, actions }, pluginOptions) => {
       }
     });
   }
+};
+
+exports.onCreateWebpackConfig = ({ actions }, pluginOptions) => {
+  const { locales = [] } = pluginOptions;
+
+  // Restricts the modules that webpack bundles in order to cover the
+  // interpolated requires in wrapPageElement, courtesy of:
+  // https://github.com/wiziple/gatsby-plugin-intl/blob/master/src/gatsby-node.js
+  let regex = new RegExp(locales.map(l => l.split('-')[0]).join('|'));
+  actions.setWebpackConfig({
+    plugins: [
+      new webpack.ContextReplacementPlugin(
+        /@formatjs[/\\]intl-relativetimeformat[/\\]dist[/\\]locale-data$/,
+        regex
+      ),
+      new webpack.ContextReplacementPlugin(
+        /@formatjs[/\\]intl-pluralrules[/\\]dist[/\\]locale-data$/,
+        regex
+      )
+    ]
+  });
 };
--- plugins/localized-pages/wrapPageElement.js
+++ plugins/localized-pages/wrapPageElement.js
@@ -3,6 +3,19 @@ import { IntlProvider } from 'react-intl';
 
 export const wrapPageElement = ({ element, props }) => {
   const { locale, messages } = props.pageContext;
+
+  let language = locale.split('-')[0];
+
+  if (!Intl.PluralRules) {
+    require('@formatjs/intl-pluralrules/polyfill');
+    require(`@formatjs/intl-pluralrules/dist/locale-data/${language}`);
+  }
+
+  if (!Intl.RelativeTimeFormat) {
+    require('@formatjs/intl-relativetimeformat/polyfill');
+    require(`@formatjs/intl-relativetimeformat/dist/locale-data/${language}`);
+  }
+
   return (
     <IntlProvider locale={locale} messages={messages}>
       {element}
