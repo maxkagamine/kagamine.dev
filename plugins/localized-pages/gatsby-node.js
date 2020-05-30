f0d037cea57c6c62592fd8817edc951101d5fb0d
Max Kagamine <max@kgmn.jp>
2020-05-30 10:39:21 -0700

    Pre-parse messages at build time

--- plugins/localized-pages/gatsby-node.js
+++ plugins/localized-pages/gatsby-node.js
@@ -1,25 +1,35 @@
+const parser = require('intl-messageformat-parser');
+const mapValues = require('lodash.mapvalues');
+
+let parsedMessages = new Map();
+
+exports.onPreInit = (_, pluginOptions) => {
+  const { locales = [], messages: messagesDir = {} } = pluginOptions;
+
+  // Pre-parse messages at build time
+  for (let locale of locales) {
+    let messages = require(`${messagesDir}/${locale}.json`);
+    let parsed = mapValues(messages, m => parser.parse(m));
+    parsedMessages.set(locale, parsed);
+  }
+};
+
 exports.onCreatePage = async ({ page, actions }, pluginOptions) => {
   const { createPage, deletePage } = actions;
-  const { locales = [], messages: messagesDir = {} } = pluginOptions;
+  const { locales = [] } = pluginOptions;
 
   // Remove automatically-generated page
   deletePage(page);
 
   // Create locale-specific pages
   for (let locale of locales) {
-    // Get messages (this is done at build-time, so no need to worry about the
-    // interpolated require)
-    // TODO: Pre-parse messages
-    let messages = require(`${messagesDir}/${locale}.json`);
-
-    // Rewrite path and pass locale & messages via context
     // TODO: Handle 404 page
     createPage({
       ...page,
       path: `/${locale}${page.path}`,
       context: {
         locale,
-        messages
+        messages: parsedMessages.get(locale)
       }
     });
   }
