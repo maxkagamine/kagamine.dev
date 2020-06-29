cd638e97df0609ecd9a5b68fc69833c9c1e6e74f
Max Kagamine <max@kgmn.jp>
2020-06-29 09:14:51 -0700

    Set cache headers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ const LOCALES = ['en', 'ja'];
 
 export const root = functions.https.onRequest((req, res) => {
   let locale = req.acceptsLanguages(LOCALES) || LOCALES[0];
-  res.set('Cache-Control', 'public, max-age=3600');
+  res.set('Cache-Control', 'public, max-age=604800');
   res.set('Vary', 'Accept-Language');
   res.redirect(`/${locale}/`);
 });
