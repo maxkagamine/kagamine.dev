b4df579f371a41509039619a5cc44ea512387c25
Max Kagamine <max@kgmn.jp>
2020-06-29 06:30:33 -0700

    Converted cover-images plugin to typescript

--- plugins/cover-images/createResolvers.ts
+++ plugins/cover-images/createResolvers.ts
@@ -1,15 +1,17 @@
-const path = require('path');
+import { CreateResolversArgs } from 'gatsby';
+import { FileSystemNode as FileNode } from 'gatsby-source-filesystem';
+import path from 'path';
 
 const COVER_NAME = 'cover';
 
-module.exports = ({ createResolvers }) => createResolvers({
+export const createResolvers = ({ createResolvers }: CreateResolversArgs) => createResolvers({
   MarkdownRemark: {
     cover: {
       type: 'File',
-      resolve(source, args, context, info) {
+      resolve(source: any, args: any, context: any, info: any): FileNode | undefined {
         // Get all png & jpg files adjacent to markdown file
         let dir = path.dirname(source.fileAbsolutePath);
-        let files = context.nodeModel.getAllNodes({ type: 'File' });
+        let files = context.nodeModel.getAllNodes({ type: 'File' }) as FileNode[];
         let images = files.filter(f =>
           path.dirname(f.absolutePath) == dir &&
           ['jpg', 'png'].includes(f.extension));
--- plugins/cover-images/gatsby-node.js
+++
@@ -1,1 +0,0 @@
-exports.createResolvers = require('./createResolvers');
