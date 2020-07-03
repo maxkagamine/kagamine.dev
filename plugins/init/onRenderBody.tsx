4d8e44e0e890dc2a84f6629242617a0ad1767771
Max Kagamine <max@kgmn.jp>
2020-07-03 03:08:42 -0700

    Fixed head components in init plugin missing keys

--- plugins/init/onRenderBody.tsx
+++ plugins/init/onRenderBody.tsx
@@ -2,8 +2,8 @@ import { RenderBodyArgs } from 'gatsby';
 import React from 'react';
 import { InitOptions } from './types';
 
-function script(js: string) {
-  return <script dangerouslySetInnerHTML={{ __html: js.replace(/\s+/gm, ' ') }} />; // Poor man's minification
+function script(key: string, js: string) {
+  return <script key={key} dangerouslySetInnerHTML={{ __html: js.replace(/\s+/gm, ' ') }} />; // Poor man's minification
 }
 
 export const onRenderBody = ({ setHeadComponents }: RenderBodyArgs, pluginOptions: InitOptions) => {
@@ -13,8 +13,8 @@ export const onRenderBody = ({ setHeadComponents }: RenderBodyArgs, pluginOption
   // Set up analytics. Client-side routing is tracked when Enhanced Measurement
   // is enabled: https://support.google.com/analytics/answer/9216061
   if (process.env.NODE_ENV == 'production') {
-    head.push(<script async src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}></script>);
-    head.push(script(`
+    head.push(<script key='init-ga-lib' async src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}></script>);
+    head.push(script('init-ga', `
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
@@ -24,7 +24,7 @@ export const onRenderBody = ({ setHeadComponents }: RenderBodyArgs, pluginOption
   }
 
   // Prevent the site from being viewed from firebase's default domains
-  head.push(script(`
+  head.push(script('init-redirect-domain', `
     if (document.location.hostname != '${domain}' && document.location.hostname != 'localhost') {
       document.location.hostname = '${domain}';
     }
